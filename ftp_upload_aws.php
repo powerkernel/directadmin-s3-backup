20070ecebd3d24219e85c066399c4ee4b7cd5826
Harry Tang <harry@powerkernel.com>
2019-02-05 22:42:30 +0200

    Update ftp_upload_aws.php

--- ftp_upload_aws.php
+++ ftp_upload_aws.php
@@ -6,28 +6,29 @@
  */
 
 /* @var $ftp_local_file string $argv[1] */
+
 /* @var $ftp_remote_file string $argv[2] */
 
 use Aws\Exception\MultipartUploadException;
 use Aws\S3\MultipartUploader;
 
 require __DIR__ . '/vendor/autoload.php';
-$conf=require __DIR__ . '/config.php';
+$conf = require __DIR__ . '/config.php';
 
-$date = date('Ymd');
+//$date = date('Ymd');
 $bucket = $conf['bucket'];
 $ftp_local_file=$argv[1];
 $ftp_remote_file=$argv[2];
 
 // S3 Client
-$opts=[
+$opts = [
     //'profile' => 'default',
     'version' => 'latest',
     'region' => $conf['region'],
     'credentials' => $conf['credentials'],
 ];
-if(!empty($conf['endpoint'])){
-	$opts['endpoint']=$conf['endpoint'];
+if (!empty($conf['endpoint'])) {
+    $opts['endpoint'] = $conf['endpoint'];
 }
 
 $client = new Aws\S3\S3Client($opts);
@@ -48,14 +49,15 @@ if ($buckets) {
 if (!$exist) {
     $client->createBucket([
         'Bucket' => $bucket,
-        'LocationConstraint' => 'ap-southeast-1',
+        'LocationConstraint' => $conf['region'],
     ]);
-// Poll the bucket until it is accessible
+
+    // Poll the bucket until it is accessible
     $client->waitUntil('BucketExists', [
         'Bucket' => $bucket
     ]);
 
-// Config auto delete
+    // add Lifecycle
     $client->putBucketLifecycleConfiguration(
         [
             'Bucket' => $bucket, // REQUIRED
@@ -78,11 +80,50 @@ if (!$exist) {
     );
 }
 
+// check and apply lifecycle
+$hasLifeCycle = false;
+try {
+    $r = $client->getBucketLifecycle(['Bucket' => $bucket]);
+    $rules = $r->get('Rules');
+    foreach ($rules as $rule) {
+        if ($rule['ID'] == 'AutoDelete') {
+            $hasLifeCycle = true;
+        }
+    }
+} catch (\Aws\S3\Exception\S3Exception $e) {
+
+}
+if (!$hasLifeCycle) {
+    // Config auto delete
+    $client->putBucketLifecycleConfiguration(
+        [
+            'Bucket' => $bucket, // REQUIRED
+            'LifecycleConfiguration' => [
+                'Rules' => [ // REQUIRED
+                    [
+                        'ID' => 'AutoDelete',
+                        'AbortIncompleteMultipartUpload' => [
+                            'DaysAfterInitiation' => 1,
+                        ],
+                        'Expiration' => [
+                            'Days' => $conf['expiration'],
+                        ],
+                        'Status' => 'Enabled',
+                        'Prefix' => ''
+                    ],
+                ],
+            ],
+        ]
+    );
+    echo "adding lifecycle";
+} else {
+    echo "lifecycle already added.";
+}
 
 // Upload
 $uploader = new MultipartUploader($client, $ftp_local_file, [
     'bucket' => $bucket,
-    'key' => date('Y-m-d').'/'.$ftp_remote_file,
+    'key' => date('Y-m-d') . '/' . $ftp_remote_file,
 ]);
 
 try {
